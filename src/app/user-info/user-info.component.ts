2b45bd734b32369b11c696ca97cef3bb8c8dc201
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-01-27 02:29:52 +0400

    initial commit

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -8,5 +8,32 @@ import json from '../../assets/data/data.json';
 })
 export class UserInfoComponent implements OnInit {
   data = json;
+  loading: boolean = false;
+  loaded: boolean = false;
+  message: boolean = true;
+  btnTxt: string = 'Load Users';
+
   ngOnInit() {}
+
+  loadData() {
+    if (this.message) {
+      this.loading = true;
+      this.loaded = false;
+      this.message = false;
+
+      new Promise<void>((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 2000);
+      }).then(() => {
+        this.loading = false;
+        this.loaded = true;
+        this.btnTxt = 'Clear';
+      });
+    } else {
+      this.loaded = false;
+      this.btnTxt = 'Load Users';
+      this.message = true;
+    }
+  }
 }
