402dc32936d64e4285264da4925ed178fbddfe80
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-01-30 11:01:58 +0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  ngOnInit() {}
-}
+export class AppComponent {}
